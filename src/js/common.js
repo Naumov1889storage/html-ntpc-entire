a3d96b2fd70c8a6b5dc1e04395a8fc3d5319f031
nebula <maksim.naumov1889@gmail.com>
2018-04-27 20:25:39 +0500

    Started mob version

--- src/js/common.js
+++ src/js/common.js
@@ -1,6 +1,14 @@
 $( function() {
     $( "#tabs" ).tabs();
 
+    $('.single-item').slick({
+        infinite: false,
+        nextArrow: '<button type="button" class="slick-next"></button>',
+        prevArrow: '<button type="button" class="slick-prev"></button>',
+        dots: true,
+
+    });
+
     $('.header__catalog .title').click(function () {
        $('.header__catalog').toggleClass('active')
     });
