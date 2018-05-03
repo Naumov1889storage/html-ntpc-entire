e380b45776e814fd4f9f82571bcfaee1cabb8b79
nebula <maksim.naumov1889@gmail.com>
2018-05-03 18:52:57 +0500

    Corrections N-1

--- dist/js/common.js
+++ dist/js/common.js
@@ -1,12 +1,18 @@
 $( function() {
     $( "#tabs" ).tabs();
 
-    $('.single-item').slick({
+    $('.single-item-desk--js').slick({
         infinite: false,
         nextArrow: '<button type="button" class="slick-next"></button>',
         prevArrow: '<button type="button" class="slick-prev"></button>',
         dots: true,
+    });
 
+    $('.single-item-mob--js').slick({
+        infinite: false,
+        nextArrow: '<button type="button" class="slick-next"></button>',
+        prevArrow: '<button type="button" class="slick-prev"></button>',
+        dots: true,
     });
 
     $('.header__catalog .title').click(function () {
